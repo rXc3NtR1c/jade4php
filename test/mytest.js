18a11ddde9357dfcddcf7337951a1623d936c22b
Brian Etherington <brian@smokingun.co.uk>
2014-08-31 19:45:05 +0100

    remove temporary test file

--- test/mytest.js
+++
@@ -1,7 +0,0 @@
-var jade = require('jade');
-require('../lib/filters')(jade);
-
-var html = jade.render(':php echo test filter!');
-//var html = jade.render('p hello world 2');
-console.log(html);
-console.log('hello world');
